e33595b46f5467f1be9ac077000b2bbf979da751
Simon Pilkington <simonpi@amazon.com>
2022-09-23 15:41:27 -0700

    Update minimum platforms.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ let swiftSettings: [SwiftSetting] = []
 let package = Package(
     name: "swift-http-client-middleware",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
+        .macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
